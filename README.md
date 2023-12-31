f951a374121965439028594720622ef78766327e
wilfreud <killlergangster@gmail.com>
2023-12-31 00:31:02 +0000

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Electron + React Boilerplate
+
+The main purpose of this repo is to provide a basic boilerplate.
+
+## Goals
+
+- Setup with Electon & React
+- Add databases layer
+  - SQLite
+  - MongoDB
+  - MySQL
+  - Postgres
+  - Maybe using prisma
+- Try to dive into security issues and stuff
+
+As I go I'll learn how to properly write a README file.
+
+Aight, let's go
---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        fullscreenable: true,
+        autoHideMenuBar: true,
+    })
+    const rootFile = path.join('index.html')
+    win.loadFile(rootFile)
+}
+
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
